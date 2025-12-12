c9f53b26bd610d8a0c3e0becefc7a6a45ffb991b
vinzenzwolf <65805275+vinzenzwolf@users.noreply.github.com>
2025-12-12 10:55:01 +0100

    Added Trainer Information to the Form

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,9 +1,16 @@
+import FormField from './FormField'
+import { useState } from 'react'
 import './SignUpForm.css'
 
 function SignUpForm() {
+  const [trainerName, setTrainerName] = useState("");
+  const [verein, setVerein] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <>
-        <div className='signUpForm'>
+        <form className="signUpForm">
 
             <section>
                 <header className="section-header">
@@ -11,11 +18,38 @@ function SignUpForm() {
                     <h2>Trainer/in</h2>
                 </div>
                 </header>
+                
+                <div className="input-grid">
+                  <FormField
+                  label='Vor/ Nachname'
+                  placeholder='Max Mustermann'
+                  value= {trainerName}
+                  onChange={(e) => setTrainerName(e.target.value)}
+                  />
+                  <FormField
+                  label='Verein'
+                  placeholder='SC Liestal'
+                  value= {verein}
+                  onChange={(e) => setVerein(e.target.value)}
+                  />
+                  <FormField
+                  label='Email'
+                  placeholder='max@scl-athletics.ch'
+                  value = {email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <FormField
+                  label='Telefonnummer'
+                  placeholder='+41 78 882 26 50'
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
             </section>
 
 
 
-        </div>
+        </form>
     </>
   )
 }
