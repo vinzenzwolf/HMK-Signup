b66fbd6368b0708b5084ab2b58b89b4df4c432c3
vinzenzwolf <65805275+vinzenzwolf@users.noreply.github.com>
2025-12-16 21:31:55 +0100

    Add Routes for admin panel and for edit tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignupPage from './pages/SignupPage'
+import AdminLogin from './pages/AdminLogin'
+import AdminDashboard from './pages/AdminDashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-
   return (
-    <>
-      <SignupPage></SignupPage>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignupPage />} />
+        <Route path="/edit/:token" element={<SignupPage />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
